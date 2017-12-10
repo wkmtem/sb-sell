7ac607341924bcf98d487f0bd16bed22530f93c4
wkm <813272417@qq.com>
2017-12-10 18:58:01 +0800

    疑问: 1、@Bean public WxMpService wxMpService() {         WxMpService wxMpService = new WxMpServiceImpl(); } public WxMpService wxOpenService() {         WxMpService wxOpenService = new WxMpServiceImpl(); } 两个相同接口的实现类，方法名不同@Autowired如何区别注入的？ @Autowired private WxMpService wxMpService; @Autowired private WxMpService wxOpenService;
    2、WxMpTemplateMessage.WxMpTemplateMessageBuilder templateMessageBuilder = WxMpTemplateMessage.builder();用法
    3、dao层用什么注解？@Repository？
    4、mybatis注解sql多表联合查询
    5、@Results与@ResultMap的使用
    6、JPA的多表联合查询
    解决
    1、@Resource按byName自动注入，@Autowired按byType自动注入；
    @Resource可用@Autowired替换
    当不想用默认的imgDataDao和payDataDao----id命名时，可用变为如下：
    @Resource(name="imgDataDao")
    private DataDao xx;
    或
    @Autowired
    @Qualifier("imgDataDao")
    private DataDao yy;

--- src/main/java/com/nsntc/sell/constant/DruidConstant.java
+++ src/main/java/com/nsntc/sell/constant/DruidConstant.java
@@ -2,28 +2,34 @@ package com.nsntc.sell.constant;
 
 /**
  * Class Name: DruidConstant
- * Package: com.nsntc.interview.constant
+ * Package: com.nsntc.sell.constant
  * Description: 德鲁伊常量
  * @author wkm
- * Create DateTime: 2017/12/10 下午5:13
+ * Create DateTime: 2017/12/10 下午6:57
  * Version: 1.0
  */
 public interface DruidConstant {
 
-    /** servlet */
     String MONITOR_URLPATTERNS = "/druid/*";
-    /** 白名单 */
+
+    /** IP白名单(没有配置或者为空,则允许所有访问) */
     String MONITOR_ALLOW = "";
-    /** 黑名单 */
+
+    /** IP黑名单(存在共同时,deny优先于allow) */
     String MONITOR_DENY = "";
+
+    /** 用户名 */
     String MONITOR_LOGINUSERNAME = "administrator";
+
+    /** 密码 */
     String MONITOR_LOGINPASSWORD = "omega";
+
     /** 禁用HTML页面上的'Reset All'功能 */
     String MONITOR_RESETENABLE = "false";
 
-    /** filter */
     String MONITOR_FILTER_FILTERNAME = "druidWebStatFilter";
     String MONITOR_FILTER_URLPATTERNS = "/*";
+
     /** 忽略资源 */
     String MONITOR_FILTER_INITPARAMS_NAME = "exclusions";
     String MONITOR_FILTER_INITPARAMS_VALUE = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*";
